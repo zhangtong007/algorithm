4e5b2704b9b2b96c3737487556b7b58a37ae1fdc
张同 <tongzh97@163.com>
2022-08-03 22:26:12 +0800

    ID:080301 Reason:leetcode code

--- src/leetcode601-800/leetcode684/func.go
+++ src/leetcode601-800/leetcode684/func.go
@@ -1,40 +1,38 @@
 package leetcode684
 
 // 冗余连接
-
 func findRedundantConnection(edges [][]int) []int {
-	// 初始化出边数组
-	// 找到n
-	n := 0
+	var n int
+	// 求解有多少个节点
 	for _, arr := range edges {
 		n = max(n, max(arr[0], arr[1]))
 	}
-	// 初始化出边数组
 	to := make([][]int, n+1)
-	// 构造出边数组
-	// 深度优先遍历出边数组，找环
+	// 最直观做法，构造出边数组，每加一条边做一次dfs，如果找到环，则为答案
 	for _, arr := range edges {
-		visited := make([]bool, n+1)
 		to[arr[0]] = append(to[arr[0]], arr[1])
 		to[arr[1]] = append(to[arr[1]], arr[0])
-		// 每次加边的时候做一次判断是否有环，有环则该边为答案
-		if dfs(to, visited, arr[0], 0) {
+		// 从arr{0}出发，作bfs，看是否有环
+		if dfs(to, make([]bool, n+1), arr[0], 0) {
 			return arr
 		}
 	}
-	return nil
+	// 题目必有环，作占位返回
+	return []int{}
 }
 
-func dfs(to [][]int, visited []bool, val, parent int) bool {
-	if visited[val] {
+func dfs(to [][]int, visited []bool, node, pre int) bool {
+	if visited[node] {
 		return true
 	}
-	visited[val] = true
-	for _, v := range to[val] {
-		if v == parent {
+	visited[node] = true
+	// 访问node的出边，作bfs
+	for _, v := range to[node] {
+		// 前一个点没必要遍历
+		if v == pre {
 			continue
 		}
-		if dfs(to, visited, v, val) {
+		if dfs(to, visited, v, node) {
 			return true
 		}
 	}
