cdde7951c53946e199f42b8c6e268c2644e329dd
张同 <tongzh97@163.com>
2024-01-07 09:09:33 +0800

    ID:20240107 Reason:rename file names

--- cmd/main.go
+++ cmd/main.go
@@ -2,27 +2,22 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 
-	quicksort "core/src/sort/quick-sort"
+	leetcode322 "algorithm/src/leetcode/0322"
+)
+
+const (
+	Byte = 1
+	KB   = Byte << 10
+	MB   = KB << 10
+	GB   = MB << 10
 )
 
 // main下面主要用于验证各个包下的demo
 func main() {
-	leetcodeTest()
-	// fmt.Println(int(1e10) <= math.MaxInt32)
-}
-
-func leetcodeTest() {
-	fmt.Println(quicksort.TestDemo(
-		randomNums(1e6),
-	))
-}
-
-func randomNums(num int) []int {
-	nums := make([]int, num)
-	for i := range nums {
-		nums[i] = rand.Intn(num)
-	}
-	return nums
+	fmt.Println(
+		leetcode322.Test(
+			[]int{}, 1,
+		),
+	)
 }
